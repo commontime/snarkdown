1daf5b9cc1061c6641930a9520b59e8e194fb7e6
RevillWeb <leon@revillweb.com>
2020-05-15 10:41:53 +0100

    Revert "Support email addresses"
    This reverts commit d6c5909c97fea4d9c4076bda9b305bfa76b57d83.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const TAGS = {
-	'': ['<em>', '</em>'],
-	_: ['<strong>', '</strong>'],
-	'~': ['<s>', '</s>'],
-	'\n': ['<br />'],
-	' ': ['<br />'],
+	'' : ['<em>','</em>'],
+	_ : ['<strong>','</strong>'],
+	'~' : ['<s>','</s>'],
+	'\n' : ['<br />'],
+	' ' : ['<br />'],
 	'-': ['<hr />']
 };
 
@@ -11,14 +11,14 @@ const TAGS = {
  *	@private
  */
 function outdent(str) {
-	return str.replace(RegExp('^' + (str.match(/^(\t| )+/) || '')[0], 'gm'), '');
+	return str.replace(RegExp('^'+(str.match(/^(\t| )+/) || '')[0], 'gm'), '');
 }
 
 /** Encode special attribute characters to HTML entities in a String.
  *	@private
  */
 function encodeAttr(str) {
-	return (str + '').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	return (str+'').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 /** Parse Markdown into an HTML String. */
@@ -42,17 +42,17 @@ export default function parse(md, prevLinks, opts) {
 		chunk, prev, token, inner, t;
 
 	function tag(token) {
-		var desc = TAGS[token.replace(/\*/g, '_')[1] || ''],
-			end = context[context.length - 1] == token;
+		var desc = TAGS[token.replace(/\*/g,'_')[1] || ''],
+			end = context[context.length-1]==token;
 		if (!desc) return token;
 		if (!desc[1]) return desc[0];
-		context[end ? 'pop' : 'push'](token);
-		return desc[end | 0];
+		context[end?'pop':'push'](token);
+		return desc[end|0];
 	}
 
 	function flush() {
 		let str = '';
-		while (context.length) str += tag(context[context.length - 1]);
+		while (context.length) str += tag(context[context.length-1]);
 		return str;
 	}
 
@@ -61,7 +61,7 @@ export default function parse(md, prevLinks, opts) {
 		return '';
 	}).replace(/^\n+|\n+$/g, '');
 
-	while ((token = tokenizer.exec(md))) {
+	while ( (token=tokenizer.exec(md)) ) {
 		prev = md.substring(last, token.index);
 		last = tokenizer.lastIndex;
 		chunk = token[0];
@@ -70,7 +70,7 @@ export default function parse(md, prevLinks, opts) {
 		}
 		// Code/Indent blocks:
 		else if (OPTIONS.code !== false && (token[3] || token[4])) {
-			chunk = (OPTIONS.replaceCodeWithEmoji) ? `${OPTIONS.replaceCodeWithEmoji}` : '<pre class="code ' + (token[4] ? 'poetry' : token[2].toLowerCase()) + '">' + outdent(encodeAttr(token[3] || token[4]).replace(/^\n+|\n+$/g, '')) + '</pre>';
+			chunk = (OPTIONS.replaceCodeWithEmoji) ? `${OPTIONS.replaceCodeWithEmoji}` : '<pre class="code '+(token[4]?'poetry':token[2].toLowerCase())+'">'+outdent(encodeAttr(token[3] || token[4]).replace(/^\n+|\n+$/g, ''))+'</pre>';
 		}
 		// > Quotes, -* lists:
 		else if (OPTIONS.quotesAndLists !== false && token[6]) {
@@ -79,12 +79,12 @@ export default function parse(md, prevLinks, opts) {
 				token[5] = token[5].replace(/^\d+/gm, '');
 			}
 			inner = parse(outdent(token[5].replace(/^\s*[>*+.-]/gm, '')), null, OPTIONS);
-			if (t === '>') t = 'blockquote';
+			if (t==='>') t = 'blockquote';
 			else {
 				t = t.match(/\./) ? 'ol' : 'ul';
 				inner = inner.replace(/^(.*)(\n|$)/gm, '<li>$1</li>');
 			}
-			chunk = '<' + t + '>' + inner + '</' + t + '>';
+			chunk = '<'+t+'>' + inner + '</'+t+'>';
 		}
 		// Images:
 		else if (OPTIONS.images !== false && token[8]) {
@@ -92,32 +92,27 @@ export default function parse(md, prevLinks, opts) {
 		}
 		// Links:
 		else if (OPTIONS.links !== false && token[10]) {
-			let url = encodeAttr(token[11] || links[prev.toLowerCase()]);
-			// Check if the URL has a protocol
-			if (url.search(/^http[s]?\:\/\//) === -1) {
-				// If it's an email then use a mailto: link instead
-				if (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(url)) {
-					url = `mailto:${url}`;
-				} else {
-					url = `http://${url}`;
-				}
-			}
-			out = out.replace('<a>', `<a href="${url}" target="_system">`);
-			// If no link text is specified just use the raw URL
-			const text = flush() || token[11];
-			chunk = text + '</a>';
-		}
+            let url = encodeAttr(token[11] || links[prev.toLowerCase()]);
+            // Check if the URL has a protocol
+            if (url.search(/^http[s]?\:\/\//) === -1) {
+                url = `http://${url}`;
+            }
+            out = out.replace('<a>', `<a href="${url}" target="_system">`);
+            // If no link text is specified just use the raw URL
+            const text = flush() || token[11];
+            chunk = text + '</a>';
+        }
 		else if (OPTIONS.links !== false && token[9]) {
 			chunk = '<a>';
 		}
 		// Headings:
 		else if (OPTIONS.headings !== false && (token[12] || token[14])) {
-			t = 'h' + (token[14] ? token[14].length : (token[13][0] === '=' ? 1 : 2));
-			chunk = '<' + t + '>' + parse(token[12] || token[15], links, OPTIONS) + '</' + t + '>';
+			t = 'h' + (token[14] ? token[14].length : (token[13][0]==='='?1:2));
+			chunk = '<'+t+'>' + parse(token[12] || token[15], links, OPTIONS) + '</'+t+'>';
 		}
 		// `code`:
 		else if (OPTIONS.code !== false && token[16]) {
-			chunk = (OPTIONS.replaceCodeWithEmoji) ? `${OPTIONS.replaceCodeWithEmoji}` : '<code>' + encodeAttr(token[16]) + '</code>';
+			chunk = (OPTIONS.replaceCodeWithEmoji) ? `${OPTIONS.replaceCodeWithEmoji}` : '<code>'+encodeAttr(token[16])+'</code>';
 		}
 		// Inline formatting: *em*, **strong** & friends
 		else if (OPTIONS.inline !== false && (token[17] || token[1])) {
